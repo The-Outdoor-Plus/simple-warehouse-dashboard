adfca4bcc2a24af294f86c68072322991000da20
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2023-08-28 15:55:16 -0700

    Added new item view, connected to monday API

--- src/views/NewItemView.vue
+++ src/views/NewItemView.vue
@@ -24,25 +24,22 @@
             <template #header="{ chooseCallback, clearCallback, files }">
                 <div class="flex flex-wrap justify-content-between align-items-center flex-1 gap-2">
                     <div class="flex gap-2">
-                        <Button @click="chooseCallback()" icon="pi pi-images" rounded outlined></Button>
+                        <Button @click="chooseCallback()" icon="pi pi-camera" rounded outlined></Button>
                         <Button @click="clearCallback()" icon="pi pi-times" rounded outlined severity="danger" :disabled="!files || files.length === 0"></Button>
                     </div>
-                    <ProgressBar :value="totalSizePercent" :showValue="false" :class="['md:w-20rem h-1rem w-full md:ml-auto', { 'exceeded-progress-bar': totalSizePercent > 100 }]"
-                        ><span class="white-space-nowrap">{{ totalSize }}B / 1Mb</span></ProgressBar
-                    >
                 </div>
             </template>
             <template #content="{ files, uploadedFiles, removeUploadedFileCallback, removeFileCallback }">
                 <div v-if="files.length > 0">
                     <h5>Pending</h5>
-                    <div class="flex flex-wrap p-0 sm:p-5 gap-5">
-                        <div v-for="(file, index) of files" :key="file.name + file.type + file.size" class="card m-0 px-6 flex flex-column border-1 surface-border align-items-center gap-3">
+                    <div class="sm:p-5">
+                        <div v-for="(file, index) of files" :key="file.name + file.type + file.size" class="w-full m-0 px-6 flex border-1 items-center gap-3 mb-4">
                             <div>
                                 <img role="presentation" :alt="file.name" :src="(file as any).objectURL" width="100" height="50" class="shadow-2" />
                             </div>
-                            <span class="font-semibold">{{ file.name }}</span>
+                            <span class="font-semibold flex-grow">{{ file.name }}</span>
                             <div>{{ formatSize(file.size) }}</div>
-                            <Badge value="Pending" severity="warning" />
+                            <!-- <Badge value="Pending" severity="warning" /> -->
                             <Button icon="pi pi-times" @click="onRemoveTemplatingFile(file, removeFileCallback, index)" outlined rounded  severity="danger" />
                         </div>
                     </div>
@@ -64,18 +61,18 @@
                 </div>
             </template>
             <template #empty>
-                <div class="flex align-items-center justify-content-center flex-column">
+                <div class="flex items-center justify-center flex-col">
                     <i class="pi pi-cloud-upload border-2 border-circle p-5 text-8xl text-400 border-400" />
                     <p class="mt-4 mb-0">Drag and drop files to here to upload.</p>
                 </div>
             </template>
         </FileUpload>
-        <!-- <FileUpload mode="basic" name="demo[]" url="./upload.php" accept="image/*" :maxFileSize="100000000" /> -->
+        <Button class="mt-8" :disabled="!poNumber" @click.prevent="createItem()">Create Item</Button>
     </div>
   </div>
 </template>
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import { useToast } from "primevue/usetoast";
 import InputNumber from 'primevue/inputnumber';
 import Dropdown from 'primevue/dropdown';
@@ -84,7 +81,13 @@ import Toast from 'primevue/toast';
 import Badge from 'primevue/badge';
 import Button from 'primevue/button';
 import FileUpload from 'primevue/fileupload';
-import ProgressBar from 'primevue/progressbar';
+import { useMutation } from '@vue/apollo-composable';
+import gql from 'graphql-tag';
+import { useRoute } from 'vue-router';
+import { create } from 'domain';
+import { defaultDataIdFromObject } from '@apollo/client';
+
+const route = useRoute();
 
 const poNumber = ref();
 const selectedTypeLabel = ref();
@@ -105,6 +108,96 @@ const toast = useToast();
 const totalSize = ref(0);
 const totalSizePercent = ref(0);
 const files = ref([]);
+const groupId = ref();
+const itemId = ref();
+const fileToUpload = ref();
+
+onMounted(() => {
+  groupId.value = route.query?.groupId || '';
+});
+
+const columnValues = computed(() => {
+  const rdyDate = `${readyDate.value.getFullYear()}-${("0" + (readyDate.value.getMonth() + 1)).slice(-2)}-${readyDate.value.getDate()}`;
+  const shppngDate = shippingDate.value ? `${shippingDate.value.getFullYear()}-${("0" + (shippingDate.value.getMonth() + 1)).slice(-2)}-${shippingDate.value.getDate()}` : '';
+  const columns = {
+    "status6": {
+      "label": selectedTypeLabel.value?.label || '',
+    },
+    "date8": {
+      "date": rdyDate,
+    },
+    "date28": {
+      "date": shppngDate,
+    },
+    "date": rdyDate,
+  }
+
+  return JSON.stringify(columns);
+});
+
+const { mutate: createItem, onDone: onCreateItemDone } = useMutation(gql`
+  mutation createItem ($groupId: String!, $itemName: String!, $columnValues: JSON!) {
+	  create_item (board_id: 2940773675, group_id: $groupId, item_name: $itemName, column_values: $columnValues) {
+		  id
+	  }
+  } 
+`, () => ({
+  variables: {
+    groupId: groupId.value,
+    itemName: `${poNumber.value}`,
+    columnValues: columnValues.value,
+  },
+}));
+
+onCreateItemDone((result) => {
+  console.log('Item Created', result.data);
+  itemId.value = result.data.create_item.id;
+  uploadFiles();
+});
+
+// const { mutate: uploadFile, onDone: onUploadFileDone } = useMutation(gql`
+//   mutation uploadFile ($file: File!, $itemId: String!) {
+//     add_file_to_column(item_id: $itemId, column_id: "files" file: $file) {
+//      id
+//     }
+//   }
+// `, () => ({
+//   variables: {
+//     file: fileToUpload.value,
+//     itemId: itemId.value,
+//   },
+//   clientId: 'mondayFileClient',
+// }));
+
+const uploadFile = async ()  => {
+  files.value.forEach(async (file) => {
+    const data = new FormData();
+    data.append('query', 'mutation add_file($file: File!) {add_file_to_column (item_id: $itemid, column_id:"files" file: $file) {id}}');
+    data.append('variables', JSON.stringify({ "itemid": itemId.value }));
+    data.append('map', JSON.stringify({ "image": "variables.file" }));
+    data.append('image', file);
+    const result = await fetch(`${import.meta.env.VITE_MONDAY_API_URL}/file` || 'https://api.monday.com/v2/file', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        Authorization : import.meta.env.VITE_MONDAY_TOKEN,
+        'API-Version' : '2023-04'
+      },
+      body: data,
+    });
+
+    if (result.status !== 200) {
+      throw new Error('Error uploading file');
+    }
+
+    const response = await result.json();
+    console.log(response);
+  });
+}
+
+const uploadFiles = async () => {
+  await uploadFile();
+}
 
 const onRemoveTemplatingFile = (file: File, removeFileCallback: Function, index: any) => {
     removeFileCallback(index);
